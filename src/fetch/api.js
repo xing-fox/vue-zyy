8d293fe1e5f2e2622c7f0ef6e787c4b1b410d266
xing-fox <450499752@qq.com>
2020-08-18 11:45:18 +0800

    ...

--- src/fetch/api.js
+++ src/fetch/api.js
@@ -4,18 +4,6 @@ import {
 
 /**
  * @param 星盘数据 
- * actiontype：1，表示新建星盘
- * userid：用户id
- * name：昵称
- * sex：性别0->女，1->男
- * cityname：城市名称
- * cityid：城市id
- * y：出生年
- * m：出生月
- * d：出生日
- * h：出生小时（0-23）
- * mi：出生分钟（0-59）
- * dst：是否采用夏时令，0->否 1->是
  */
 export const getData_XP = (param) => {
   return Post('/index.php?main_page=xingpan_handler', param)
